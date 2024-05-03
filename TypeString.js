9a8d9fd94be5ce02439a8ec15e88c1e024aa95a5
Joao pedro <pedro.pereira.joao@escola.pr.gov.br>
2024-05-03 12:37:30 -0300

    aula alura

---
+++ TypeString.js
@@ -0,0 +1,25 @@
+// tipo string 
+//cadeia de caracteres (simbolos)
+//exemplo
+//frases
+//textos
+
+const string1 = "Jose";
+const string2 = "Valim";
+
+//concatenar
+//juntar duas strings
+
+const nome = string1 + " " + string2;
+
+console.log(nome);
+
+//exemplo
+
+const nome2 = "joao";
+const sobrenome = "pereira";
+const idade = 30;
+
+console.log("nome: " + nome2);
+console.log("sbrenome: " + sobrenome);
+console.log("idade: " + idade);
---
+++ tipo-number.js
@@ -0,0 +1,13 @@
+// Tipo number
+
+// Declaração variavel number
+
+const numero1 = 3;
+const numero2 = 4;
+
+const soma = numero1 + numero2;
+const subtracao = numero1 + numero2;
+
+// mostra as operaçoes 
+
+console.log(soma);
