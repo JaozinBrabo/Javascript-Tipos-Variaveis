9a64c0c8bd1794343b09a2e58004f868a5e2c5db
Joao pedro <pedro.pereira.joao@escola.pr.gov.br>
2024-05-07 09:52:11 -0300

    Feat:add Boolean

---
+++ typeBoolean.js
@@ -0,0 +1,17 @@
+// Boolean
+//Verdadeiro ou falso
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+// === Compara tipo e valor
+console.log(primeiroNumero === segundoNumero);
+
+// declarar uma variavel com um valor booleano
+const ativo = false;
+let ativo2 = true; //vai permitir a mudança do valor da variavel
+
+const nome = " jaozin";
+const sobrenome = "pereira"
+
+console.log(nome === sobrenome)
